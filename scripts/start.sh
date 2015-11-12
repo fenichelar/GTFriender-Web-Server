b707ec5e2553300ada0fe4ddcce0de419ee8376e
Alec Fenichel <alec.fenichel@gmail.com>
2015-11-12 11:48:01 -0500

    Update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,10 @@
 
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 
-if [[ $* == *--dev* ]]
+if [[ $* == *--log* ]]
   then
     forever -w --sourceDir $SCRIPT_PATH/.. start app.js
+    forever logs 0 -f
   else
-    forever -w --sourceDir $SCRIPT_PATH/.. start app.js --prod
+    forever -w --sourceDir $SCRIPT_PATH/.. start app.js
 fi
