fe35dc16bfff7ae19c80d1311c83316eb451111f
Alec Fenichel <alec.fenichel@gmail.com>
2016-01-31 18:46:40 -0500

    Update documentation test

--- test/unit/controllers/example.test.js
+++ test/unit/controllers/example.test.js
@@ -1,9 +1,9 @@
 var request = require('supertest');
 
-describe('GET /docs/', function(){
-  it('respond with documentation', function(done){
+describe('GET /docs/index.html', function() {
+  it('respond with documentation', function(done) {
     request(sails.hooks.http.app)
-      .get('/docs/')
+      .get('/docs/index.html')
       .expect(200, done);
   });
 });
