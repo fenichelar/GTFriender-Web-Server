ff534805fa7874f7328e1066de82fdb4d223183e
Alec Fenichel <alec.fenichel@gmail.com>
2016-02-11 19:56:46 -0500

    Remove passport.js config settings

--- config/passport.js
+++ config/passport.js
@@ -12,15 +12,6 @@
 
 module.exports.passport = {
 
-  google: {
-    name: 'Google',
-      protocol: 'oauth2',
-    strategy: require('passport-google-oauth').OAuth2Strategy,
-    options: {
-      scope: ['profile', 'email']
-    }
-  }
-
   /*local: {
     strategy: require('passport-local').Strategy
   },
@@ -29,6 +20,17 @@ module.exports.passport = {
     strategy: require('passport-http').BasicStrategy,
     protocol: 'basic'
   },
+
+  google: {
+    name: 'Google',
+      protocol: 'oauth2',
+    strategy: require('passport-google-oauth').OAuth2Strategy,
+    options: {
+      clientID: 'your-client-id',
+      clientSecret: 'your-client-secret',
+      scope: ['profile', 'email']
+    }
+  }
 
   twitter: {
     name: 'Twitter',
