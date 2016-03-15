e42c82f9341794f6ebba3c5be2ee4ca43bdd6bfa
Alec Fenichel <alec.fenichel@gmail.com>
2016-03-15 17:05:43 -0400

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,8 +20,9 @@ echo "<h1>Deploy Status</h1>"
 echo "<div>"
 echo "<h3>Stopping Server...</h3>"
 echo "<p>"
-cd $REPO_PATH && ps aux | grep '[/]var/www/GTFriender-Web-Server/app.js'
-cd $REPO_PATH && sudo kill $(ps aux | grep '[/]var/www/GTFriender-Web-Server/app.js' | awk '{print $2}')
+ps aux | grep '[G]TFriender-Web-Server'
+sudo killall node
+ps aux | grep '[G]TFriender-Web-Server'
 echo "</p>"
 echo "<b>Done.</b>"
 echo "</div>"
